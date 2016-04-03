85b1c5d2bf83b222bd0577a1c18316b8d7bbb937
gohv <hristovsotirovgeorgi@gmail.com>
2016-04-03 22:08:58 +0300

    Test
    test class

---
+++ src/letter/bukvar/test.java
@@ -0,0 +1,14 @@
+package letter.bukvar;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		Letter letter = new Letter();
+		letter.setMap();
+		System.out.println(letter.letters.get('v'));
+		
+
+	}
+
+}
