63cb7d9d2aa96896bdf1c0fe058f61ce3f6d5546
gohv <hristovsotirovgeorgi@gmail.com>
2016-04-03 22:06:01 +0300

    Bukvar
    This will contain the main method and provide the main logic.

---
+++ src/bukvarMain/bukvar/Bukvar.java
@@ -0,0 +1,17 @@
+package bukvarMain.bukvar;
+
+import letter.bukvar.*;
+
+public class Bukvar {
+
+	private char bukvarLetter;
+
+	public void setLetter(char letter) {
+
+		this.bukvarLetter = letter;
+
+	}
+
+	
+
+}
