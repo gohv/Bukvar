bffbf34a1b01b0a0a96d3c825fa12dc6faf89693
gohv <hristovsotirovgeorgi@gmail.com>
2016-04-03 22:09:37 +0300

    Words Class
    This class will provide more words to a called letter.

---
+++ src/words/bukvar/Words.java
@@ -0,0 +1,162 @@
+package words.bukvar;
+
+import letter.bukvar.*;
+
+/**
+ * This class contains words - explanation to letters
+ *
+ */
+public class Words {
+
+	private String a = "Автомобил";
+	private String b = "Бомбон";
+	private String w = "Велосипед";
+	private String g = "Гъба";
+	private String d = "Динозавър";
+	private String e = "Елен";
+	private String v = "Жаба";
+	private String z = "Зима";
+	private String i = "Игла";
+	private String j = "ѝод";
+	private String k = "Кон";
+	private String l = "Лимон";
+	private String m = "Минзухар";
+	private String n = "Носорог";
+	private String o = "Октопод";
+	private String p = "Паун";
+	private String r = "Рак";
+	private String s = "Сабя";
+	private String t = "Трамвай";
+	private String u = "Усмивка";
+	private String f = "Футболист";
+	private String h = "Хвърчило";
+	private String c = "Цедка";
+	private String ch = "Чадър";
+	private String sh = "Шал";
+	private String st = "Щъркел";
+	private String y = "Ъглометър";
+	private String x = "Шофьор";
+	private String iu = "Ютия";
+	private String q = "Ягода";
+
+	public void setA(String a) {
+		this.a = a;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+
+	public void setW(String w) {
+		this.w = w;
+	}
+
+	public void setG(String g) {
+		this.g = g;
+	}
+
+	public void setD(String d) {
+		this.d = d;
+	}
+
+	public void setE(String e) {
+		this.e = e;
+	}
+
+	public void setV(String v) {
+		this.v = v;
+	}
+
+	public void setZ(String z) {
+		this.z = z;
+	}
+
+	public void setI(String i) {
+		this.i = i;
+	}
+
+	public void setJ(String j) {
+		this.j = j;
+	}
+
+	public void setK(String k) {
+		this.k = k;
+	}
+
+	public void setL(String l) {
+		this.l = l;
+	}
+
+	public void setM(String m) {
+		this.m = m;
+	}
+
+	public void setN(String n) {
+		this.n = n;
+	}
+
+	public void setO(String o) {
+		this.o = o;
+	}
+
+	public void setP(String p) {
+		this.p = p;
+	}
+
+	public void setR(String r) {
+		this.r = r;
+	}
+
+	public void setS(String s) {
+		this.s = s;
+	}
+
+	public void setT(String t) {
+		this.t = t;
+	}
+
+	public void setU(String u) {
+		this.u = u;
+	}
+
+	public void setF(String f) {
+		this.f = f;
+	}
+
+	public void setH(String h) {
+		this.h = h;
+	}
+
+	public void setC(String c) {
+		this.c = c;
+	}
+
+	public void setCh(String ch) {
+		this.ch = ch;
+	}
+
+	public void setSh(String sh) {
+		this.sh = sh;
+	}
+
+	public void setSt(String st) {
+		this.st = st;
+	}
+
+	public void setY(String y) {
+		this.y = y;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+
+	public void setIu(String iu) {
+		this.iu = iu;
+	}
+
+	public void setQ(String q) {
+		this.q = q;
+	}
+
+}
